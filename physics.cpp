cea9b283b0cc8338c234de1384d1b7c3a0a50451
yukamurata27 <yukamurata27@gmail.com>
2020-02-11 22:57:59 -0800

    Implement collision detection and external force field

--- physics.cpp
+++ physics.cpp
@@ -260,18 +260,22 @@ void SpringSystem(struct world * jello, struct point f[8][8][8])
       }
 }
 
-void CollisionDetectionHelper(struct world * jello, struct point p, struct point &f)
+void CollisionDetectionHelper(struct world * jello, struct point indices, struct point &f)
 {
-  //int Ax = indexA.x, Ay = indexA.y, Az = indexA.z;
+  int px = indices.x, py = indices.y, pz = indices.z;
   //int Bx = indexB.x, By = indexB.y, Bz = indexB.z;
   double Llen, diff, dotProdRes, R=0.;
-  point A, B, L, FTmp, vecDiff;
+  point pOut, pSurface, L, FTmp, vecDiff;
 
-  //pCPY(jello->p[Ax][Ay][Az], A);
-  //pCPY(jello->p[Bx][By][Bz], B);
-  //pDIFFERENCE(A, B, L);
-
-  /*
+  pCPY(jello->p[px][py][pz], pOut);
+  pCPY(jello->p[px][py][pz], pSurface);
+  if (pSurface.x >= 2.) pSurface.x = 2.;
+  else if (pSurface.x <= -2.) pSurface.x = -2.;
+  if (pSurface.y > 2.) pSurface.y = 2.;
+  else if (pSurface.y <= -2.) pSurface.y = -2.;
+  if (pSurface.z >= 2.) pSurface.z = 2.;
+  else if (pSurface.z <= -2.) pSurface.z = -2.;
+  pDIFFERENCE(pOut, pSurface, L);
   pLENGTH(L, Llen);
 
   // Hook's law
@@ -280,17 +284,16 @@ void CollisionDetectionHelper(struct world * jello, struct point p, struct point
   pSUM(f, FTmp, f);
 
   // Damping force
-  point vec0(0, 0, 0);
-  pDIFFERENCE(jello->v[Ax][Ay][Az], vec0, vecDiff); // vecDiff = vA - vB
+  point vec0 = {0, 0, 0};
+  pDIFFERENCE(jello->v[px][py][pz], vec0, vecDiff); // vecDiff = vA - vB
   pDOTPRODUCT(vecDiff, L, dotProdRes);
   pMULTIPLY(L, -jello->dCollision*dotProdRes/Llen/Llen, FTmp);
   pSUM(f, FTmp, f);
-  */
 }
 
 bool isOutOfBox(struct point p)
 {
-  return p.x < -4 || p.y < -4 || p.z < -4 || 4 < p.x || 4 < p.y || 4 < p.z;
+  return p.x <= -2. || p.y <= -2. || p.z <= -2. || 2. <= p.x || 2. <= p.y || 2. <= p.z;
 }
 
 void CollisionDetection(struct world * jello, struct point f[8][8][8])
@@ -307,8 +310,51 @@ void CollisionDetection(struct world * jello, struct point f[8][8][8])
 
         if (isOutOfBox(jello->p[i][j][k]))
         {
-          return;
+          point indices = {i, j, k};
+          CollisionDetectionHelper(jello, indices, f[i][j][k]);
         }
+      }
+}
+
+void ForceField(struct world * jello, struct point f[8][8][8])
+{
+  if (jello->resolution == 0) return;
+  
+  int i, j, k;
+
+  for (i=0; i<=7; i++)
+    for (j=0; j<=7; j++)
+      for (k=0; k<=7; k++)
+      {
+        // Initialize
+        pINIT(f[i][j][k]);
+        double interval = jello->resolution/8.; // Or divided by 7????
+        double xpos = i/interval, ypos = j/interval, zpos = k/interval;
+        int xindices[2] = {floor(xpos), ceil(xpos)};
+        int yindices[2] = {floor(ypos), ceil(ypos)};
+        int zindices[2] = {floor(zpos), ceil(zpos)};
+        double alpha = (xpos-floor(xpos))/interval;
+        double beta = (ypos-floor(ypos))/interval;
+        double gamma = (zpos-floor(zpos))/interval;
+        //cout << "Result: " << alpha << ", " << beta << ", " << gamma << endl;
+
+        point Fxyz, Fx1yz, Fxy1z, Fxyz1, Fx1y1z, Fx1yz1, Fxy1z1, Fx1y1z1;
+        pCPY(jello->forceField[xindices[0] * jello->resolution * jello->resolution + yindices[0] * jello->resolution + zindices[0]], Fxyz);
+        pCPY(jello->forceField[xindices[1] * jello->resolution * jello->resolution + yindices[0] * jello->resolution + zindices[0]], Fx1yz);
+        pCPY(jello->forceField[xindices[0] * jello->resolution * jello->resolution + yindices[1] * jello->resolution + zindices[0]], Fxy1z);
+        pCPY(jello->forceField[xindices[0] * jello->resolution * jello->resolution + yindices[0] * jello->resolution + zindices[1]], Fxyz1);
+        pCPY(jello->forceField[xindices[1] * jello->resolution * jello->resolution + yindices[1] * jello->resolution + zindices[0]], Fx1y1z);
+        pCPY(jello->forceField[xindices[1] * jello->resolution * jello->resolution + yindices[0] * jello->resolution + zindices[1]], Fx1yz1);
+        pCPY(jello->forceField[xindices[0] * jello->resolution * jello->resolution + yindices[1] * jello->resolution + zindices[1]], Fxy1z1);
+        pCPY(jello->forceField[xindices[1] * jello->resolution * jello->resolution + yindices[1] * jello->resolution + zindices[1]], Fx1y1z1);
+
+        point interpF = {
+          alpha*beta*gamma*Fx1y1z1.x + (1-alpha)*beta*gamma*Fxy1z1.x + alpha*(1-beta)*gamma*Fx1yz1.x + alpha*beta*(1-gamma)*Fx1y1z.x + (1-alpha)*(1-beta)*gamma*Fxyz1.x + (1-alpha)*beta*(1-gamma)*Fxy1z.x + alpha*(1-beta)*(1-gamma)*Fx1yz.x + (1-alpha)*(1-beta)*(1-gamma)*Fxyz.x,
+          alpha*beta*gamma*Fx1y1z1.y + (1-alpha)*beta*gamma*Fxy1z1.y + alpha*(1-beta)*gamma*Fx1yz1.y + alpha*beta*(1-gamma)*Fx1y1z.y + (1-alpha)*(1-beta)*gamma*Fxyz1.y + (1-alpha)*beta*(1-gamma)*Fxy1z.y + alpha*(1-beta)*(1-gamma)*Fx1yz.y + (1-alpha)*(1-beta)*(1-gamma)*Fxyz.y,
+          alpha*beta*gamma*Fx1y1z1.z + (1-alpha)*beta*gamma*Fxy1z1.z + alpha*(1-beta)*gamma*Fx1yz1.z + alpha*beta*(1-gamma)*Fx1y1z.z + (1-alpha)*(1-beta)*gamma*Fxyz1.z + (1-alpha)*beta*(1-gamma)*Fxy1z.z + alpha*(1-beta)*(1-gamma)*Fx1yz.z + (1-alpha)*(1-beta)*(1-gamma)*Fxyz.z
+        };
+        pSUM(f[i][j][k], interpF, f[i][j][k])
+        //cout << "Result: " << f[i][j][k].x << ", " << f[i][j][k].y << ", " << f[i][j][k].z << endl;
       }
 }
 
@@ -321,6 +367,7 @@ void computeAcceleration(struct world * jello, struct point a[8][8][8])
   point FAll[8][8][8], FSpring[8][8][8], FPenalty[8][8][8], FForceField[8][8][8];
   SpringSystem(jello, FSpring);
   CollisionDetection(jello, FPenalty);
+  ForceField(jello, FForceField);
 
   // Initialize variables
   for (i=0; i<=7; i++)
@@ -336,6 +383,8 @@ void computeAcceleration(struct world * jello, struct point a[8][8][8])
       for (k=0; k<=7; k++)
       {
         pSUM(FAll[i][j][k], FSpring[i][j][k], FAll[i][j][k]);
+        pSUM(FAll[i][j][k], FPenalty[i][j][k], FAll[i][j][k]);
+        pSUM(FAll[i][j][k], FForceField[i][j][k], FAll[i][j][k]);
       }
 
   // a = FAll / m
